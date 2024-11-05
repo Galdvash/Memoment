73939085af8fdb2379dd3ad85478384c0169628b
Galdvash <Galdvash59@gmail.com>
2024-11-05 17:47:40 +0200

    update app.mjs

--- BackEnd/app.mjs
+++ BackEnd/app.mjs
@@ -9,20 +9,10 @@ import selfieRoutes from "./routes/selfieRoutes.mjs";
 import faceRoutes from "./routes/faceRoutes.mjs";
 import excelRoutes from "./routes/excelRoutes.mjs";
 import twilioRoutes from "./routes/twilioRoutes.mjs";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html")); // או 'build'
-});
 
 const corsOptions = {
   origin: "http://localhost:3000", // כתובת צד הלקוח שלך
