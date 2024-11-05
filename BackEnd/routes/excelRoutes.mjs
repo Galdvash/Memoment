c939bd313eecb01e00a4a23b0411b132f4367d3e
MyProjects <145988104+Galdvash@users.noreply.github.com>
2024-11-05 02:30:12 +0200

    Delete BackEnd directory

--- BackEnd/routes/excelRoutes.mjs
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { upload, uploadExcelFile } from "../controllers/excelController.mjs";
-
-const router = express.Router();
-
-// Route for uploading Excel file
-router.post("/upload", upload.single("file"), uploadExcelFile);
-export default router;
