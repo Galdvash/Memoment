78a10f0369373ba9b192ea3b0e245e09b34c7d6d
Galdvash <Galdvash59@gmail.com>
2024-11-13 02:33:44 +0200

    ג

--- BackEnd/middleware/authMiddleware.mjs
+++ BackEnd/middleware/authMiddleware.mjs
@@ -4,17 +4,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/UserModel.mjs";
 
 export const protect = async (req, res, next) => {
-  let token;
-
-  if (req.cookies && req.cookies.token) {
-    token = req.cookies.token;
-  } else if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  }
-
+  const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ message: "Not authorized, no token" });
   }
