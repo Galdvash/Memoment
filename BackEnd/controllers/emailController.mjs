7a954406158044b1086c695fe893c17a66085d8d
Galdvash <Galdvash59@gmail.com>
2024-11-19 22:49:55 +0200

    re

--- BackEnd/controllers/emailController.mjs
+++ BackEnd/controllers/emailController.mjs
@@ -6,9 +6,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
